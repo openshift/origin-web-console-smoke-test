f4cd65e56d1baab1ef38b008fafa7a6935bc137f
Jakub Hadvig <jhadvig@redhat.com>
2018-04-16 15:54:22 +0200

    Exit when unable get self-sign cert

--- smoke-test.sh
+++ smoke-test.sh
@@ -15,11 +15,18 @@ host_with_port=$(echo "$url" | sed 's/\/.*$//')
 host=$(echo "$host_with_port" | sed 's/:.*$//')
 port=$(echo "$host_with_port" | grep : | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g')
 
-os::log::info "Getting certificate for ${CONSOLE_URL}"
-timeout 30 google-chrome --no-sandbox --headless --disable-gpu --dump-dom "$CONSOLE_URL" &>/dev/null
-echo 'GET /' | timeout 30 openssl s_client -showcerts -connect "$host:$port" | openssl x509 -outform PEM > /tmp/console-e2e.pem
-os::log::info "Storing obtained certificate"
-certutil -d "sql:$HOME/.pki/nssdb" -A -n console -t Pu,, -i /tmp/console-e2e.pem
+if [ -n "$SELF_SIGN_CERT" ]; then
+  os::log::info "Getting self-signed certificate for ${CONSOLE_URL}"
+  timeout 30 google-chrome --no-sandbox --headless --disable-gpu --dump-dom "$CONSOLE_URL" &>/dev/null
+  echo 'GET /' | timeout 30 openssl s_client -showcerts -connect "$host:$port" | openssl x509 -outform PEM > /tmp/console-e2e.pem
+  if [[ $? -eq 0 ]]; then
+    os::log::info "Storing obtained certificate"
+    certutil -d "sql:$HOME/.pki/nssdb" -A -n console -t Pu,, -i /tmp/console-e2e.pem
+  else
+    os::log::error "Failed to obtain certificate"
+    exit 1
+  fi
+fi
 
 # protractor will need this to login...
 service_account_token=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
